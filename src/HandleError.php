a4056eded38e3a3975bdb4dfe7347db6694f95de
bruno <brunopirizoliveira@gmail.com>
2023-02-22 10:44:39 -0300

    First commit

---
+++ src/HandleError.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SZH\HandleError;
+
+class HandleError {
+
+    private $dict;
+
+    public function __construct($jsonFile) {
+        // $this->dict = (new Dictionary())->getErrors();
+        $this->dict = $jsonFile;
+    }
+
+    public function handleError($serverError)
+    {
+        $handledError = $this->dict[$serverError];        
+
+        header("Content-Type: application/json");
+        return json_encode($handledError);  
+    }
+
+}
