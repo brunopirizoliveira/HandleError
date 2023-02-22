e6773fc24b5971ce4168b500896ab7b837c1de9d
bruno <brunopirizoliveira@gmail.com>
2023-02-22 17:36:01 -0300

    Exception custom

--- src/CustomException.php
+++ src/CustomException.php
@@ -9,6 +9,9 @@ class CustomException extends Exception
 
     public function __construct($message, $code = 0, Exception $previous = null) {
         parent::__construct($message, $code, $previous);
+        $customError = new HandleError();
+        $customError->setTrace($this->getTraceAsString());
+        $customError->handleError($this);
     }
 
     public function customMessage() {
