98e95a1a0355954184421c8d70e91baeab820ff0
bruno <brunopirizoliveira@gmail.com>
2023-02-23 17:06:44 -0300

    Add createError

--- src/HandleError.php
+++ src/HandleError.php
@@ -7,21 +7,15 @@ class HandleError {
     private $dict;
     private $trace;
     private $message;
+    private $error;
+    private $subcode;
+    private $code;
+    private $context;
 
     public function __construct() {
         // $this->dict = (new Dictionary())->getErrors();
         $jsonString = file_get_contents(storage_path().DIRECTORY_SEPARATOR.env('HANDLE_ERROR_JSON'));
         $this->dict = json_decode($jsonString, true);
-    }
-
-    public function setMessage($message)
-    {
-        $this->message = $message;
-    }
-
-    public function setTrace($trace)
-    {
-        $this->trace = $trace;
     }
 
     public function handleError($statusCode)
@@ -31,6 +25,49 @@ class HandleError {
         $handledError["message"] = $this->message;
         $handledError["trace"] = $this->trace;
         return json_encode($handledError);  
+    }
+
+    public function createError()
+    {
+        $handledError["message"] = $this->message;
+        $handledError["trace"] = $this->trace;
+        $handledError["error"] = $this->error;
+        $handledError["context"] = $this->context;
+        $handledError["code"] = $this->code;
+        $handledError["subcode"] = $this->subcode;
+
+        return $handledError;
+    }
+
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function setError($error)
+    {
+        $this->error = $error;
+    }
+
+    public function setTrace($trace)
+    {
+        $this->trace = $trace;
+    }
+
+    public function setContext($context)
+    {
+        $this->context = $context;
+    }
+
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    public function setSubcode($subcode)
+    {
+        $this->subcode = $subcode;
     }
 
 }
