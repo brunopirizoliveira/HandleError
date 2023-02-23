9bb7bd89539df3e416eae3ea0f45c52cf6fb3db1
bruno <brunopirizoliveira@gmail.com>
2023-02-22 23:14:22 -0300

    Exception custom

--- src/CustomException.php
+++ src/CustomException.php
@@ -9,12 +9,15 @@ class CustomException extends Exception
 
     public function __construct($message, $code = 0, Exception $previous = null) {
         parent::__construct($message, $code, $previous);
-        $customError = new HandleError();
-        $customError->setTrace($this->getTraceAsString());
-        $customError->handleError($this);
+
     }
 
     public function customMessage() {
-        echo "Custom Message will go here\n";
+        
+        $customHandleError = new \SZH\HandleError\HandleError();
+        $customHandleError->setTrace($this->getTraceAsString());
+        $handleError = $customHandleError->handleError($this->getCode());
+        
+        return json_encode($handleError);  
     }
 }
